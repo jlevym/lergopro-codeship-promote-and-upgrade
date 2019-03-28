4e20f58edf4e04a51ffb914819a127139fb2200d
Jeffrey <4lergo@gmail.com>
2019-03-28 17:33:35 +0200

    Make uploaded files public : --acl public-read-write

--- scripts/promote_lergopro.sh
+++ scripts/promote_lergopro.sh
@@ -25,10 +25,10 @@ if [[ ! "$build_id" =~ ^[0-9]+$ ]]; then
 fi
 echo copying files in S3 from ${build_id} to 'latest'
 
-aws s3 cp s3://lergopro-backups/artifacts/build-lergopro-${build_id}/jobs/build-lergopro/${build_id}/build.id s3://lergopro-backups/artifacts/latest/build.id
-aws s3 cp s3://lergopro-backups/artifacts/build-lergopro-${build_id}/jobs/build-lergopro/${build_id}/install.sh s3://lergopro-backups/artifacts/latest/install.sh
-aws s3 cp s3://lergopro-backups/artifacts/build-lergopro-${build_id}/jobs/build-lergopro/${build_id}/lergo-ri-0.0.0.tgz s3://lergopro-backups/artifacts/latest/lergo-ri-0.0.0.tgz
-aws s3 cp s3://lergopro-backups/artifacts/build-lergopro-${build_id}/jobs/build-lergopro/${build_id}/lergo-ui-0.0.0.tgz s3://lergopro-backups/artifacts/latest/lergo-ui-0.0.0.tgz
+aws s3 cp s3://lergopro-backups/artifacts/build-lergopro-${build_id}/jobs/build-lergopro/${build_id}/build.id s3://lergopro-backups/artifacts/latest/build.id --acl public-read-write
+aws s3 cp s3://lergopro-backups/artifacts/build-lergopro-${build_id}/jobs/build-lergopro/${build_id}/install.sh s3://lergopro-backups/artifacts/latest/install.sh --acl public-read-write
+aws s3 cp s3://lergopro-backups/artifacts/build-lergopro-${build_id}/jobs/build-lergopro/${build_id}/lergo-ri-0.0.0.tgz s3://lergopro-backups/artifacts/latest/lergo-ri-0.0.0.tgz --acl public-read-write
+aws s3 cp s3://lergopro-backups/artifacts/build-lergopro-${build_id}/jobs/build-lergopro/${build_id}/lergo-ui-0.0.0.tgz s3://lergopro-backups/artifacts/latest/lergo-ui-0.0.0.tgz --acl public-read-write
 
 
 echo promote completed succesfully
